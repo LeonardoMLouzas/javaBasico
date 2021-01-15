56e28ff1b7ff5517264ae17f06dd664c6cf90635
LeonardoMLouzas <leomoreira1998@gmail.com>
2021-01-15 17:02:12 -0300

    teste

---
+++ src/atividadesAula33/Atividade01.java
@@ -0,0 +1,17 @@
+package atividadesAula33;
+
+//Escreva  uma  classe  para  representar  uma  lâmpada.  Desenvolva  métodos   para  ligar,  desligar  a  lampada. 
+
+public class Atividade01 {
+
+    private boolean ligada = false;
+
+    public boolean getLigada() {
+        return ligada;
+    }
+
+    public void setLigada(boolean ligada) {
+
+        this.ligada = ligada;
+    }
+}
---
+++ src/atividadesAula33/Main.java
@@ -0,0 +1,17 @@
+package atividadesAula33;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+/*******************************************************************/
+        Atividade01 lampada = new Atividade01();
+
+        lampada.setLigada(true);
+        System.out.println(lampada.getLigada());
+/*******************************************************************/
+
+        
+
+    }
+}
